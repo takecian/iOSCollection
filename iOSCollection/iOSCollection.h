718cda53b07f16a98bb897514dd0e8da106495ae
takecian <takecian@gmail.com>
2018-12-02 15:59:39 +0900

    first commit

---
+++ iOSCollection/iOSCollection.h
@@ -0,0 +1,19 @@
+//
+//  iOSCollection.h
+//  iOSCollection
+//
+//  Created by TAKESHI FUJIKI on 2018/12/02.
+//  Copyright © 2018 takecian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSCollection.
+FOUNDATION_EXPORT double iOSCollectionVersionNumber;
+
+//! Project version string for iOSCollection.
+FOUNDATION_EXPORT const unsigned char iOSCollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSCollection/PublicHeader.h>
+
+
